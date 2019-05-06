0bed42f6c5170dced817462b0c6067e49f01d273
changmin <changmin043@gmail.com>
2019-05-06 07:42:49 +0000

    [Add, Refector] 저장된 이미지 보여주기, 이미지 파일 위치 변수로 선언

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ from flask import Flask, send_from_directory, request
 import json
 import datetime
 
+image_dir = "images"
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return send_from_directory(directory="", filename="index.html")
 
 @app.route("/upload", methods=['POST'])
 def upload():
     image_file = request.files["file"]
-    path = "images/" + image_file.filename
+    path = "{0}/{1}".format(image_dir, image_file.filename)
     image_file.save(path)
     return json.dumps({"path":path})
 
-
+@app.route("/images/<name>")
+def image(name):
+    return send_from_directory(directory=image_dir, filename=name)
 
 
