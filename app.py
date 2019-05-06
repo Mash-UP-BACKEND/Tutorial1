a7bd8b3980170ecc4f62a39c371e7b206e6a8afd
changmin <changmin.kim@worksmobile.com>
2019-05-06 18:16:51 +0900

    [Add] 업로드 결과를 저장하도록 함

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import json
 import datetime
 
 image_dir = "images"
+log_dir = "logs/upload-{0}.log".format(datetime.datetime.now())
 
 app = Flask(__name__)
+
+def write_log(result, path):
+    with open(log_dir, 'a+') as log_file:
+        log_content = "{0}|{1}|{2}\n".format(datetime.datetime.now(), result, path) 
+        log_file.write(log_content)
 
 @app.route("/")
 def index():
@@ -12,10 +18,22 @@ def index():
 
 @app.route("/upload", methods=['POST'])
 def upload():
+    if "file" not in request.files:
+        result = "Cannot find file"
+        write_log(result, None)
+        return "", 400
+
     image_file = request.files["file"]
+
+    if image_file.filename == "":
+        result = "File name is empty"
+        write_log(result, None)
+        return "", 400
+
     path = "{0}/{1}".format(image_dir, image_file.filename)
     image_file.save(path)
-    return json.dumps({"path":path})
+    write_log("Success", path)
+    return json.dumps({"path":path}), 200
 
 @app.route("/images/<name>")
 def image(name):
