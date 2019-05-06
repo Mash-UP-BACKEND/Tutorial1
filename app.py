5b11ac6b39e2875d1a2e221611d77edb71ab9eee
changmin <changmin043@gmail.com>
2019-05-06 07:26:32 +0000

    [ADD] image file upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request
+import json
 import datetime
 
 app = Flask(__name__)
@@ -6,3 +7,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return send_from_directory(directory="", filename="index.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    image_file = request.files["file"]
+    path = "images/" + image_file.filename
+    image_file.save(path)
+    return json.dumps({"path":path})
+
+
+
+
